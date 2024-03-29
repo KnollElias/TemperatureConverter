a9fac07712fc028718f71f405b4ca61cebb925fa
KnollElias <107244530+KnollElias@users.noreply.github.com>
2024-03-29 16:37:45 +0100

    Add Tests for transformation

--- src/transform.rs
+++ src/transform.rs
@@ -16,3 +16,28 @@ pub fn convertToFahrenheit(input_temerature: f32) -> f32 {
     let fahrenheit_rounded = roundTemeperature(fahrenheit.clone());
     fahrenheit_rounded
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_convert_to_celsius() {
+        assert_eq!(convertToCelsius(32.0), 0.0);
+        assert_eq!(convertToCelsius(212.0), 100.0);
+        assert_eq!(convertToCelsius(INPUT_TEMPERATURE_VAR), -6.36);
+    }
+
+    #[test]
+    fn test_convert_to_fahrenheit() {
+        assert_eq!(convertToFahrenheit(0.0), 32.0);
+        assert_eq!(convertToFahrenheit(100.0), 212.0);
+        assert_eq!(convertToFahrenheit(-6.36), 20.55);
+    }
+
+    #[test]
+    fn test_round_temperature() {
+        assert_eq!(roundTemeperature(32.75111), 32.75);
+        assert_eq!(roundTemeperature(32.259), 32.26);
+    }
+}
